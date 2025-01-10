5de2ad4285a5ae12fe809f7ccb9b9ba7f097f36d
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-01-10 12:30:22 +0530

    Added TextField

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,28 +15,61 @@ class _HomePageState extends State<HomePage> {
     Message(text: "Hi", isUser: true),
     Message(text: "Hello how are you?", isUser: false),
     Message(text: "Nothing much here", isUser: true),
-    Message(text: "Ok", isUser: true),
+    Message(text: "Ok", isUser: false),
   ];
+
+final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView.builder(
-            itemCount: _messages.length,
-            itemBuilder: (context, index) {
-              final message = _messages[index];
-              return ListTile(
-                title: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Text(
-                    message.text,
-                    style: TextStyle(color: message.isUser ? Colors.white: Colors.black),
-                  ),
-                ),
-              );
-            }));
+
+   return Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    final message = _messages[index];
+                    return ListTile(
+                      title: Align(
+                        alignment: message.isUser
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            color: message.isUser ? Colors.blue : Colors.grey[400],
+                            borderRadius: message.isUser
+                                ? BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20),
+                                  )
+                                : BorderRadius.only(
+                                    topRight: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20),
+                                  ),
+                          ),
+                          child: Text(
+                            message.text,
+                            style: TextStyle(
+                                color: message.isUser ? Colors.white : Colors.black),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+               controller: _controller,
+              ),
+            )
+          ],
+        ));
   }
 }
