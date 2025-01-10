08cfb1e46fb55940a7b57b474b6f581b01868a58
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-01-10 12:43:50 +0530

    Finished designing the input button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:ai_chat/message.dart';
 import 'package:flutter/material.dart';
@@ -18,58 +18,84 @@ class _HomePageState extends State<HomePage> {
     Message(text: "Ok", isUser: false),
   ];
 
-final TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
-   return Scaffold(
+    return Scaffold(
         body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: _messages.length,
-                  itemBuilder: (context, index) {
-                    final message = _messages[index];
-                    return ListTile(
-                      title: Align(
-                        alignment: message.isUser
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        child: Container(
-                          padding: EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            color: message.isUser ? Colors.blue : Colors.grey[400],
-                            borderRadius: message.isUser
-                                ? BorderRadius.only(
-                                    topLeft: Radius.circular(20),
-                                    bottomRight: Radius.circular(20),
-                                    bottomLeft: Radius.circular(20),
-                                  )
-                                : BorderRadius.only(
-                                    topRight: Radius.circular(20),
-                                    bottomLeft: Radius.circular(20),
-                                    bottomRight: Radius.circular(20),
-                                  ),
-                          ),
-                          child: Text(
-                            message.text,
-                            style: TextStyle(
-                                color: message.isUser ? Colors.white : Colors.black),
-                          ),
-                        ),
+      children: [
+        Expanded(
+          child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ListTile(
+                  title: Align(
+                    alignment: message.isUser
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: message.isUser ? Colors.blue : Colors.grey[400],
+                        borderRadius: message.isUser
+                            ? BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20),
+                              )
+                            : BorderRadius.only(
+                                topRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
                       ),
-                    );
-                  }),
+                      child: Text(
+                        message.text,
+                        style: TextStyle(
+                            color:
+                                message.isUser ? Colors.white : Colors.black),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 32, top: 16.0, left: 16, right: 16),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(32),
+              boxShadow: 
+              [  BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3)
+              )]
+            
             ),
-
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-               controller: _controller,
-              ),
-            )
-          ],
-        ));
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: "Write your message...",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20)
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                IconButton(onPressed: () {}, icon: Icon(Icons.send))
+              ],
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
